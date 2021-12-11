c0addf246cf295a00952a54d7223954393d5b9da
maxkohn <kohn.max@gmail.com>
2021-12-11 09:49:05 -0500

    removed delete function

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,12 +3,12 @@ const router = require('express').Router();
 //deconstruct notes json and require it in the app
 //deconstructing notes ACTUALLY broke it
 const notes = require('../../db/db.json');
-const {addNote, deleteNote} = require('../../lib/note');
+const addNote = require('../../lib/note');
 
-let results = notes;
 
 //when you visit the notes, get the current list and send the json results
 router.get('/notes', (req,res) => {
+    let results = notes;
     console.log('in notes');
     res.json(results);
 });
@@ -20,7 +20,7 @@ router.post('/notes', (req,res) => {
     res.json(noteAdded);
 })
 
-router.delete('/notes/:id', (req,res) => {
+/*router.delete('/notes/:id', (req,res) => {
     console.log('in delete route');
     console.log(req.params.id);
     const deleteNotes = deleteNote(req.params.id, notes);
@@ -30,7 +30,7 @@ router.delete('/notes/:id', (req,res) => {
     console.log(results);
     res.json(deleteNotes);
 })
-/*router.delete('/notes/:id', (req,res) => {
+router.delete('/notes/:id', (req,res) => {
     console.log(req.body);
     const deleteNotes = deleteNote(req.body, notes);
     //res.json(req.body);
